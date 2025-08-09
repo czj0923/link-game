f2084fd3857dd591f8efb88a86ff13b223197bee
czj0923 <45352719+czj0923@users.noreply.github.com>
2025-08-09 06:50:30 +0000

    Deploying to gh-pages from @ czj0923/game-box@2ba56faeb8ac01a1cc8c219167d34bfca8f0d52c 🚀

--- src/pages/test/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>{{ state }}</div>
-</template>
-<script lang="ts" setup>
-import { useStorage } from '@vueuse/core';
-const state = useStorage('link_game_records', []);
-</script>
--- src/stores/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineStore } from 'pinia';
-
-export const useMainStore = defineStore('main', {
-  state: () => ({
-    ratio: 1 // 屏幕宽度在840px以下时，缩小画布比例
-  })
-});
